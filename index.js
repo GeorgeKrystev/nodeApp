d7a5145e578ae25e4674802547ef33d2f1421ece
georgecr0ss <george.krystew@gmail.com>
2016-07-02 17:41:37 +0300

    adding cats module

--- index.js
+++ index.js
@@ -12,12 +12,15 @@
 //express declaration
 var express = require('express');
 var	app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-	res.send('Hello world');		
-	// res.json({hello: "world"});
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
+var cats = require('./cats.js');
+ 
 var server = app.listen(3000, function() {
 	console.log('Server running on port 3000');
 })
